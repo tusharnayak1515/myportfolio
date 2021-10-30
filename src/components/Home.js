ab479a3ed979547b023d83d6027b3df16f4d8835
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2021-10-31 00:50:24 +0530

    1234

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 
 const Home = () => {
+  console.log(process.env.PUBLIC_URL);
   return (
     <div id={styles.home}>
       <h1>Hello, I'm <span>Tushar Ranjan Nayak</span>.</h1>
       <h2>I'm a Full-Stack Web Developer.</h2>
-      <Link to="/projects"><button>View My Work</button></Link>
+      <Link to={process.env.PUBLIC_URL + '/projects'}><button>View My Work</button></Link>
     </div>
   );
 };
